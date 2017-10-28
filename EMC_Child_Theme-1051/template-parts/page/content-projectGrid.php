a06779d91bc3a728e15a70b3fc56e7bb0eb23729
amelia.payne <ameliappenrose@gmail.com>
2017-10-28 16:15:31 -0400

    Inigrated project page grid with ACF so it is dinamically genrated in a repeater field. You can now add as many grid elements as you want in the GUI. As aloyes make sure to import latest ACF layout file so everything dosent break

--- EMC_Child_Theme-1051/template-parts/page/content-projectGrid.php
+++ EMC_Child_Theme-1051/template-parts/page/content-projectGrid.php
@@ -4,82 +4,40 @@
 
 
 <div class="container-fluid projects-page"> 
-	<h1>Projects:</h1>
-	<div class="row">
-	<div class="col-sm-4">
-		<img src="http://www.wellcomeimageawards.org/jpegs/58956/B0010980_main.jpg"> 
-		<h2>Project 1</h2>
-		<p>
-		Tip: Go to our CSS Responsive Web Design Tutorial to learn more about responsive web design and grids.
-		</p>
-	</div>
 
-	<div class="col-sm-4">
-		<img src="http://www.wellcomeimageawards.org/jpegs/58956/B0010980_main.jpg">
-		<h2>Project 1</h2>
-		<p>
-		Tip: Go to our CSS Responsive Web Design Tutorial to learn more about responsive web design and grids.
-		</p>
-	</div>
-
-	<div class="col-sm-4">
-		<img src="http://www.wellcomeimageawards.org/jpegs/58956/B0010980_main.jpg">
-		<h2>Project 1</h2>
-		<p>
-		Tip: Go to our CSS Responsive Web Design Tutorial to learn more about responsive web design and grids.
-		</p>
-	</div>
-	</div>
-
-	<div class="row">
-		<div class="col-sm-4">
-			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ87qapmgUogqbOOZpe8vOy6S62cMXaz6ly9UJJwUfkQaY01GTf0Q">
-			<h2>Project 1</h2>
+	<h1><?php the_sub_field('title')?></h1>
+	
+	<?php 
+	// check if the repeater field has rows of data
+	if( have_rows('project_grid_repeater') ):
+	//Istilise i to 0, i is used to keep track of itterations and insert a new row tag every three runs
+		$i = 0;
+		// loop through the rows of data
+		while ( have_rows('project_grid_repeater') ) : the_row();
+		
+		//Get the image the user entered and put it into image (its a relational array)
+		$image = get_sub_field('project_grid_image');
+		
+		//Check if this run is a multible of 3 if so insert new row div wrapper
+			if(($i % 3) == 0):?>
+				<div class="row">
+			<?php endif?>
+			
+			<!--All info that creates each item -->
+			<div class="col-sm-4">
+			<img src="<?php echo $image['url']; ?>" alt ="<?php echo $image['alt']?>"> 
+			<h2><?php the_sub_field('project_title'); ?></h2>
 			<p>
-			Tip: Go to our CSS Responsive Web Design Tutorial to learn more about responsive web design and grids.
+			<?php the_sub_field('project_description'); ?>
 			</p>
-		</div>
+			</div>
+<?php $i++ ?>
+	<?php 
+		
+    endwhile;
+	endif;
+	?>
 
-		<div class="col-sm-4">
-			<img src="http://www.wellcomeimageawards.org/jpegs/58956/B0010980_main.jpg">
-			<h2>Project 1</h2>
-			<p>
-			Tip: Go to our CSS Responsive Web Design Tutorial to learn more about responsive web design and grids.
-			</p>
-		</div>
-
-		<div class="col-sm-4">
-			<img src="http://www.wellcomeimageawards.org/jpegs/58956/B0010980_main.jpg">
-			<h2>Project 1</h2>
-			<p>
-			Tip: Go to our CSS Responsive Web Design Tutorial to learn more about responsive web design and grids.
-			</p>
-		</div>
-	</div>
-
-	<div class="row">
-		<div class="col-sm-4">
-			<img src="http://www.wellcomeimageawards.org/jpegs/58956/B0010980_main.jpg">
-			<h2>Project 1</h2>
-			<p>
-			Tip: Go to our CSS Responsive Web Design Tutorial to learn more about responsive web design and grids.
-			</p>
-		</div>
-
-		<div class="col-sm-4">
-			<img src="http://www.wellcomeimageawards.org/jpegs/58956/B0010980_main.jpg">
-			<h2>Project 1</h2>
-			<p>
-			Tip: Go to our CSS Responsive Web Design Tutorial to learn more about responsive web design and grids.
-			</p>
-		</div>
-
-		<div class="col-sm-4">
-			<img src="http://www.wellcomeimageawards.org/jpegs/58956/B0010980_main.jpg">
-			<h2>Project 1</h2>
-			<p>
-			Tip: Go to our CSS Responsive Web Design Tutorial to learn more about responsive web design and grids.
-			</p>
-		</div>
+	
 
 	</div>
