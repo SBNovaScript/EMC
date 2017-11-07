58536f5bcb2ea94d3645eb2bfdcfcd71bec05f02
Steven Baumann <steven.baumann@mymail.champlain.edu>
2017-11-07 16:02:36 -0500

    Added image selection for Three Image Layout.

--- EMC_Child_Theme-1051/template-parts/page/content-projects.php
+++ EMC_Child_Theme-1051/template-parts/page/content-projects.php
@@ -5,19 +5,19 @@
 <div id="three_col_layout">
 <div class="w3-cell-row abc">
   <div class="w3-container w3-cell w3-cell-top w3-mobile col">
-    <img src="https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg">
-    <h1><?php echo get_theme_mod('project1Title') ?> <span class="date">(<?php echo get_theme_mod('project1Date') ?>)</span></h1>
+    <img src="<?php echo the_sub_field('three_project_layout_images_one') ?>">
+    <h1><?php echo get_theme_mod('project1Title'); ?> <span class="date">(<?php echo get_theme_mod('project1Date') ?>)</span></h1>
     <p><?php echo get_theme_mod('project1')?></p>
   </div>
   
   <div class="w3-container w3-mobile w3-cell col">
-  <img src="https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg">
+  <img src="<?php echo the_sub_field('three_project_layout_images_two') ?>">
   <h1> <?php echo get_theme_mod('project2Title') ?> <span class="date">(<?php echo get_theme_mod('project2Date') ?>)</span></h1>
     <p><?php echo get_theme_mod('project2')?></p>
   </div>
   
   <div class="w3-container w3-mobile w3-cell col">
-  <img src="https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg">
+  <img src="<?php echo the_sub_field('three_project_layout_images_three') ?>">
   <h1><?php echo get_theme_mod('project3Title') ?> <span class="date">(<?php echo get_theme_mod('project3Date') ?>)</span></h1>
     <p><?php echo get_theme_mod('project3')?></p>
   </div>
