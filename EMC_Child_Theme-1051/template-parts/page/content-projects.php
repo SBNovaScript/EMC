e172b310a3ed155c42541de6e6934cc11d26eb0c
Steven Baumann <steven.baumann@mymail.champlain.edu>
2017-11-10 17:12:42 -0500

    added fade in jQuery and Two Project layout repeater.

--- EMC_Child_Theme-1051/template-parts/page/content-projects.php
+++ EMC_Child_Theme-1051/template-parts/page/content-projects.php
@@ -1,6 +1,7 @@
 <!-- TODO These colums get totally messed up if amount of text is diffrent or image demitions are diffrent, Fix this issue-->
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <?php
 
@@ -10,8 +11,9 @@ if(have_rows('three_field_repeater') ):
 	
 		?>
 	
+
 	
-	<div id="three_col_layout">
+	<div class="three_col_layout">
 <div class="w3-cell-row abc">
   <div class="three_project w3-container w3-cell w3-cell-top w3-mobile col">
     <img class="three_projects_img" src="<?php the_sub_field('three_projects_image_one') ?>">
