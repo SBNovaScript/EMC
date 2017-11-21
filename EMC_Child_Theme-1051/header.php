57e61da3dfb3c09aa2e2dcb9016d457adbbce6c3
Steven Baumann <steven.baumann@mymail.champlain.edu>
2017-11-21 15:01:59 -0500

    added meta tag.

--- EMC_Child_Theme-1051/header.php
+++ EMC_Child_Theme-1051/header.php
@@ -18,6 +18,7 @@
 
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
+<meta name="robots" content="noindex">
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
