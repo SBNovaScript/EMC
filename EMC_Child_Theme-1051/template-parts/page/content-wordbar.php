d9967a8e87382cf272ab0d692c84c994109bc824
Steven Baumann <steven.baumann@mymail.champlain.edu>
2017-12-01 15:09:23 -0500

    Removed wordbar buttons, cleaned css, and added in the subtitle for wordbar.

--- EMC_Child_Theme-1051/template-parts/page/content-wordbar.php
+++ EMC_Child_Theme-1051/template-parts/page/content-wordbar.php
@@ -1,10 +1,16 @@
 <!-- This is included on the home page to make the nice looking word blocks -->
 
 <div id="EII_bar">
-<h1 class="big_text">
+<h1 class="big_text" style="margin-bottom: 20px;"> <!-- Overwrote the big_text margin-bottom here, to include the subtitle text closer. -->
 	 <?php echo the_sub_field('wordBar_text') ?>
 </h1>
+<h4>
+	<?php echo the_sub_field('subtitle_text') ?>
+</h4>
+
+<!-- Jen wanted these removed
 <a class="word" id="block1" href="#idtext-0">EMERGE</a>
 <a class="word" id="block2" href="#idtext-1">INNOVATE</a>
 <a class="word" id="block3" href="#idtext-2">INSPIRE</a>
+-->
 </div>
